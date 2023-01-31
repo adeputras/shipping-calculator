d2bfae04e58bccdcb34914c2c466e139ef82c82d
Ade Putra Susilo <adeputrasusilo@Macbooks-MacBook-Pro.local>
2023-01-31 15:51:08 +0700

    add copyright

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Form from '../components/form';
 import './main.scss';
 const Main = () => {
-
   return (
     <div className="main">
-      <h1>Packing Due date and Shipping ETA Calculation</h1>
+      <div className="title-section">
+        <h1>Packing Due date and Shipping ETA Calculation</h1>
+      </div>
       <Form />
+      <div className="copyright">
+        <p><small>Copyright © 2023 Hadi Santoso. All rights reserved.</small> </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
